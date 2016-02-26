5cb2f93ef0984526e1ba164465bdf9c56939e94f
Cory Benfield <lukasaoz@gmail.com>
2016-02-26 14:33:44 +0000

    Pull OS X code out to submodule file.

--- src/osx.rs
+++ src/osx.rs
@@ -1,29 +1,30 @@
-extern crate libc;
+use security_framework::certificate::SecCertificate;
+use security_framework::policy::SecPolicy;
+use security_framework::secure_transport::ProtocolSide;
+use security_framework::trust::SecTrust;
 
-#[cfg(macos)]
-extern crate core_foundation;
-#[cfg(macos)]
-extern crate security_framework;
-#[cfg(windows)]
-extern crate crypt32;
-#[cfg(windows)]
-extern crate winapi;
-
-macro_rules! fail_on_error {
-    ($e:expr) => {
-        match $e {
-            Ok(s) => s,
+pub fn validate_cert_chain(encoded_certs: Vec<&[u8]>, hostname: &str) -> bool {
+    let mut certs = Vec::new();
+    for encoded_cert in encoded_certs {
+        let cert = SecCertificate::from_der(encoded_cert);
+        match cert {
+            Ok(cert) => certs.push(cert),
             Err(_) => return false,
         }
     }
+
+    let ssl_policy = fail_on_error!(SecPolicy::for_ssl(ProtocolSide::Client, hostname));
+    let trust = fail_on_error!(SecTrust::create_with_certificates(&certs[..], &[ssl_policy]));
+
+    // Deliberately swallow errors here: any error is likely to do with the
+    // hostname or cert chain, and if those are invalid then by definition the
+    // cert does not validate.
+    match trust.evaluate() {
+        Ok(result) => result.success(),
+        Err(_) => false
+    }
 }
 
-#[cfg(windows)]
-pub mod windows;
-#[cfg(macos)]
-pub mod os_x;
-
-#[cfg(macos)]
 #[cfg(test)]
 mod test {
     use os_x::validate_cert_chain;
