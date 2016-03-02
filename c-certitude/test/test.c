dec502da1e55307b11805ef876c005c5e7b67165
Cory Benfield <lukasaoz@gmail.com>
2016-03-02 13:57:04 +0000

    Test C lib on Travis.

---
+++ c-certitude/test/test.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+extern uint32_t validate_cert_chain(uint8_t **encoded_certs,
+                                    size_t *cert_sizes,
+                                    size_t cert_count,
+                                    const char *hostname);
+
+#define CERT_COUNT 3
+#define BUFFER_SIZE 4096
+
+static char *leaf_cert = "../rust-certitude/fixtures/certifi/leaf.crt";
+static char *first_intermediate = "../rust-certitude/fixtures/certifi/first-intermediate.crt";
+static char *second_intermediate = "../rust-certitude/fixtures/certifi/second-intermediate.crt";
+static char *correct_hostname = "certifi.io";
+static char *incorrect_hostname = "certitude.io";
+
+
+
+int main(int argc, char **argv) {
+    char *filenames[CERT_COUNT] = {leaf_cert, first_intermediate, second_intermediate};
+    uint8_t *certs[CERT_COUNT];
+    size_t sizes[CERT_COUNT];
+    uint32_t validation_result;
+
+    // Read the certs.
+    for (int i = 0; i < CERT_COUNT; i++) {
+        FILE *file = NULL;
+        uint8_t *cert_data = malloc(BUFFER_SIZE);
+        size_t read_count;
+
+        assert(cert_data != NULL);
+
+        file = fopen(filenames[i], "r");
+        if (file == NULL) {
+            fprintf(stderr, "Cannot open file %d!\n", i);
+            exit(1);
+        }
+
+        read_count = fread(cert_data, 1, BUFFER_SIZE, file);
+        if (read_count == BUFFER_SIZE) {
+            fprintf(stderr, "File too long!\n");
+            exit(1);
+        }
+
+        fclose(file);
+
+        // Resize the buffer, we don't need the extra data.
+        cert_data = realloc(cert_data, read_count);
+        assert(cert_data != NULL);
+
+        certs[i] = cert_data;
+        sizes[i] = read_count;
+    }
+
+    // Just test two basic cases: one with the correct hostname, one without.
+    validation_result = validate_cert_chain(certs, sizes, CERT_COUNT, correct_hostname);
+    if (validation_result == 1) {
+        printf("Correct validated chain.\n");
+    } else {
+        printf("Unexpected result, code %d\n", validation_result);
+        exit((int)validation_result);
+    }
+
+    validation_result = validate_cert_chain(certs, sizes, CERT_COUNT, incorrect_hostname);
+    if (validation_result == 2) {
+        printf("Correctly rejected hostname.\n");
+    } else {
+        printf("Unexpected result, code %d\n", validation_result);
+        exit((int)validation_result);
+    }
+
+    // Do some cleanup.
+    for (int j = 0; j < CERT_COUNT; j++) {
+        free(certs[j]);
+    }
+
+    return 0;
+}
---
+++ travis/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# For the main lib, we build and test normally.
+cd rust-certitude
+cargo build --verbose
+cargo test --verbose
+
+# For the C ABI, we build in either debug or release mode, then build the C
+# code appropriately and run it.
+# TODO: Use an env var to separate these two notions!
+CARGO_FLAGS=""
+OPTIMIZATION=""
+TARGET="debug"
+
+if [[ "${RELEASE}" = true ]]; then
+    CARGO_FLAGS="--release"
+    OPTIMIZATION="-O1"
+    TARGET="release"
+
+cd ../c-certitude
+cargo build --verbose "${CARGO_FLAGS}"
+clang -fsanitize=address -L "target/${TARGET}" -framework Security -framework CoreFoundation -lSystem -lc -lm -lc_certitude "${OPTIMIZATION}" test/test.c
+./a.out
